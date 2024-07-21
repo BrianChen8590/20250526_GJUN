93f5a6d12a27092e135883cc8e8f9619f191c6a4
BrianChen <abc85909859@gmail.com>
2024-07-21 10:48:44 +0800

    20240721-1
    springboot+mybatis

--- bootWeb/2_spMyBatis/src/main/java/com/example/demo/dao/MemberMapper.java
+++ bootWeb/2_spMyBatis/src/main/java/com/example/demo/dao/MemberMapper.java
@@ -2,17 +2,37 @@ package com.example.demo.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.example.demo.model.Member;
 
 @Mapper // 產生bean物件
 public interface MemberMapper {
 
-	// @Insert()
+	@Insert("insert into member(name,username,password,address,phone) "
+			+ "values(#{name},#{username},#{password},#{address},#{phone})")
 	void add(Member m);
 
-	// @Select()
+	@Select("select * from member")
 	List<Member> selectAll();
+
+	@Select("select * from member where username=#{username} and password=#{password}")
+	List<Member> selectMember(String username, String password);
+
+	@Select("select * from member where username=#{username} ")
+	List<Member> selectUsername(String username);
+
+	@Select("select * from member where id=#{id}")
+	List<Member> selectById(int id);
+
+	@Update("update member set name=#{name},address=#{address} where id=#{id}")
+	void update(Member m);
+
+	@Delete("delete from member where id=#{id}")
+	void delete(int id);
 
 }
